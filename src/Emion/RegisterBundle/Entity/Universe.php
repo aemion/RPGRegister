53230e587aa51aec737ef58352624b02cbe4e71f
Alexandre Emion <alexandre.emion@gmail.com>
2015-12-26 13:22:30 +0100

    Ajout entité universe

---
+++ src/Emion/RegisterBundle/Entity/Universe.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Emion\RegisterBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Universe
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Emion\RegisterBundle\Entity\UniverseRepository")
+ */
+class Universe
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="text")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Universe
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Universe
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
